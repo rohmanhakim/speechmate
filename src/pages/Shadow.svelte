7bbc1efc818db7b64744fc75f7118db639a11aa1
Muhammad Arif Rohman Hakim <rohmanhakim@live.com>
2025-11-23 09:46:49 +0700

    Add basic shadowing page

---
+++ src/pages/Shadow.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<article class="card space-y-4 p-4 flex flex-col h-screen">
+    <div class="card p-4 flex-grow bg-gray-800">
+        <p class="text-2xl">
+            Hey, can i take your order?
+        </p>
+    </div>
+    <button class="btn preset-outlined-primary-500 flex-none">
+        Play
+    </button>
+</article>
