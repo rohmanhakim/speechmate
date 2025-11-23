593a6422e36a0eddb204765ac8e41f8a51d530a2
Muhammad Arif Rohman Hakim <rohmanhakim@live.com>
2025-11-23 15:02:26 +0700

    Integrate Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arifrohman.speechmate',
+  appName: 'speechmate',
+  webDir: 'dist'
+};
+
+export default config;
