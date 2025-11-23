4edf6e622ebc20324342128091cc272f194dbe34
Muhammad Arif Rohman Hakim <rohmanhakim@live.com>
2025-11-23 08:48:24 +0700

    Integrate TypeScript

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import './app.css';
   import svelteLogo from './assets/svelte.svg';
   import viteLogo from '/vite.svg';
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,25 @@
-<script>
-  let count = $state(0)
+<script lang="ts">
+  let count: number = $state(0);
+  let squared: number = $state(0);
+
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
+
+  const square = (a: number) => {
+    return a * a;
+  };
 </script>
 
-<button onclick={increment}  class="btn preset-outlined-primary-500">
-  count is {count}
-</button>
+<article class="card space-y-4 p-4">
+  <button onclick={increment} class="btn preset-outlined-primary-500">
+    count is {count}
+  </button>
+
+  <button
+    onclick={() => (squared = square(count))}
+    class="btn preset-outlined-primary-500"
+  >
+    squared is {squared}
+  </button>
+</article>
